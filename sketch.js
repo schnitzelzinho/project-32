dcec30377208a4acde424e430ece21082d3b5f84
schnitzelzinho <104394163+schnitzelzinho@users.noreply.github.com>
2022-10-27 18:05:09 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,11 +29,11 @@ function setup() {
 
   button.mousePressed(blow);
 
-  //buttonPressed(blow);
+  //;buttonPressed(blow);
   
-  //button = mousePressed(blow);
+  //;button = mousePressed(blow);
   
-  //button.mousePressed();
+  //;button.mousePressed();
 
 }
 
@@ -48,13 +48,13 @@ function draw() {
 
 function blow() {
 
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
+  //;Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
 
   Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
   
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0.05, y:0.05});
+  //;Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0.05, y:0.05});
   
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:-0.05, y:0});
+  //;Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:-0.05, y:0});
 
 }
 
